eb4e29d1a8c8e8581507e748d4e110cb41525a87
satrio92 <kukuhsatrio920@gmail.com>
2023-01-28 11:05:40 +0700

    update server

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,7 +14,15 @@ app.use(cors());
 // parse application/json
 app.use(bodyParser.json());
 
-
+app.post('/send-message', (req, res) => {
+    let oyy = `nama : ${req.body.name} \nemail : ${req.body.email} \npesannya adalah : \n${req.body.message}`
+    client.messages.create({
+        from: 'whatsapp:+14155238886',
+        to: 'whatsapp:+6282335283181',
+        body: oyy
+    }).then(message => console.log(message.sid));
+    res.send('Data received');
+});
 
 app.get('/', (req, res) => {
     res.send('Welcome to Satrio Server!');
