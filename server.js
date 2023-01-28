f4c3db8e717460dd0ed9d4aff0e5580f92b569cf
satrio92 <kukuhsatrio920@gmail.com>
2023-01-28 10:36:21 +0700

    update authtoken

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const accountSid = 'AC0870d499d521669e07919bb86af72a07';
-const authToken = '163c8b79b30cec52929f04477a495a6e';
+// const accountSid = 'MGd328e4da35a30f1f8b97f746a0cf1587';
+const authToken = 'dd54d04bd188d5513691305e26481bd4';
 const client = require('twilio')(accountSid, authToken);
 
 // parse application/x-www-form-urlencoded
@@ -15,8 +16,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.post('/send-message', (req, res) => {
-    let oyy = `
-    nama : ${req.body.name} \nemail : ${req.body.email} \npesannya adalah : \n${req.body.message}`
+    let oyy = `nama : ${req.body.name} \nemail : ${req.body.email} \npesannya adalah : \n${req.body.message}`
     client.messages.create({
         from: 'whatsapp:+14155238886',
         to: 'whatsapp:+6282335283181',
