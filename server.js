c84af732549c0d3def1847f10ca895d58c7b4ab8
Kukuh Satrio <kukuhsatrio920@gmail.com>
2023-05-12 13:17:16 +0700

    add shortlink function

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const shortid = require('shortid');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -6,6 +8,19 @@ require('dotenv').config();
 const accountSid = process.env.ACCOUNT_SID;
 const authToken = process.env.AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
+
+mongoose.connect('mongodb+srv://kukuh_satrio:kukuhsatrio123@cluster0.jf4gbun.mongodb.net/kukuhsatrio?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
+
+// Define schema for shortlink
+const shortlinkSchema = new mongoose.Schema({
+    fullUrl: String,
+    shortUrl: String,
+});
+
+// Create model for shortlink
+const Shortlink = mongoose.model('Shortlink', shortlinkSchema);
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,6 +42,42 @@ app.post('/send-message', (req, res) => {
 app.get('/', (req, res) => {
     res.send('Welcome to Satrio Server!');
 });
+
+// Endpoint for creating shortlinks
+app.post('/shortlinks', async (req, res) => {
+    const fullUrl = req.body.fullUrl;
+    const shortUrl = shortid.generate();
+  
+    // Save shortlink to database
+    const shortlink = new Shortlink({
+      fullUrl: fullUrl,
+      shortUrl: shortUrl,
+    });
+    await shortlink.save();
+  
+    // Return shortlink to client
+    const response = {
+      fullUrl: fullUrl,
+      shortUrl: shortUrl,
+    };
+    res.send(response);
+  });
+  
+  // Endpoint for redirecting shortlinks
+  app.get('/:shortUrl', async (req, res) => {
+    const shortUrl = req.params.shortUrl;
+  
+    // Find shortlink in database
+    const shortlink = await Shortlink.findOne({ shortUrl: shortUrl });
+  
+    if (!shortlink) {
+      return res.status(404).send('Shortlink not found');
+    }
+  
+    // Redirect to full URL
+    // res.redirect(shortlink.fullUrl);
+    res.send(shortlink.fullUrl);
+  });
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
